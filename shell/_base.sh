c486f530448c87ef5e3095c80cb9551455c59fb5
Yuri Pereira Constante <ypconstante@gmail.com>
2021-03-30 00:49:02 -0300

    Messages on file begin and end

--- shell/_base.sh
+++ shell/_base.sh
@@ -19,9 +19,41 @@ set -e
 sudo true
 set +e
 
-cd "$(dirname "$0")"
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
-ASSETS_DIR=$(realpath ../assets)
+export ASSETS_DIR=$(realpath ../assets)
+export CURRENT_SCRIPT="${BASH_SOURCE[1]}"
 
 source /etc/os-release
+
 source "$ASSETS_DIR/base--env"
+
+#################################### PRINT #####################################
+
+# my:file_run
+my:file_run_begin() {
+    local file="$1"
+    my:file_run_echo "FILE BEGIN: ${file}"
+    echo ''
+}
+
+my:file_run_end() {
+    local file="$1"
+    my:file_run_echo "FILE END: ${file}"
+    echo ''
+}
+
+my:file_run_echo() {
+    local message="$1"
+    echo "$(tput setab 4)$(tput setaf 0)${message}$(tput el)$(tput sgr0)"
+}
+
+################################################################################
+
+my:file_run_begin "$CURRENT_SCRIPT"
+
+function on_exit {
+    my:file_run_end "$CURRENT_SCRIPT"
+}
+
+trap on_exit EXIT
