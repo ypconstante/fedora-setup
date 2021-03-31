5ea60fb5e930b7c836845ac3b96304012e0eaafb
Yuri Pereira Constante <ypconstante@gmail.com>
2021-03-30 23:40:57 -0300

    Add terminal tools

---
+++ shell/terminal_tools.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
+
+my:step_begin "install cli tools"
+my:dnf_install xclip
+my:dnf_install xdotool
+my:step_end
+
+my:step_begin "install tldr"
+my:dnf_install tealdeer
+tldr tldr &> /dev/null
+tldr --update
+my:step_end
