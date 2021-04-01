b953c79508e3fb035ffa8db494c2fe32a62478bf
Yuri Pereira Constante <ypconstante@gmail.com>
2021-04-01 01:25:51 -0300

    Add chromium

---
+++ shell/browser_chrome.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
+
+my:step_begin "install chromium"
+my:dnf_install chromium
+my:step_end
+
+my:step_begin "add temporary chromium to menu"
+menu_file="$XDG_DATA_HOME/applications/chromium-temporary.desktop"
+my:link_file "$ASSETS_DIR/browser_chrome--temp-profile.desktop" "$menu_file"
+chmod +x "$menu_file"
+my:step_end
