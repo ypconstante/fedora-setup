bcf23ccebeb714b0ace75def62c1402cb7c6fb7c
Yuri Pereira Constante <ypconstante@gmail.com>
2021-04-02 01:21:56 -0300

    Ensure asdf plugins are updated

--- shell/dev_asdf.sh
+++ shell/dev_asdf.sh
@@ -9,3 +9,7 @@ my:step_end
 my:step_begin "config asdf"
 my:link_file "$ASSETS_DIR/dev_asdf--rc" "$ASDF_CONFIG_FILE"
 my:step_end
+
+my:step_begin "update plugins"
+asdf plugin-update --all
+my:step_end
