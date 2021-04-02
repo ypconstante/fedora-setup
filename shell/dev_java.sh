6a014d6924ee1380c11c7dfb9a02d1b3448858c0
Yuri Pereira Constante <ypconstante@gmail.com>
2021-04-02 01:05:11 -0300

    Add java

---
+++ shell/dev_java.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
+
+my:step_begin "install java"
+my:asdf_add_plugin java
+my:asdf_install_and_set_global java "$(asdf list-all java | grep openjdk-8 | grep -v 'openj9' | tail -1)"
+my:step_end
+
+my:step_begin "install maven"
+my:asdf_add_plugin maven
+my:asdf_install_and_set_global maven "$(asdf list-all maven | grep -v '[a-z]' | tail -1)"
+my:step_end
