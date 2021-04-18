625f75c9fe1d0ad41ffe382aee916817d19dfefc
Yuri Pereira Constante <ypconstante@gmail.com>
2021-04-18 09:26:37 -0300

    Fix old headers

--- shell/utility_flatseal.sh
+++ shell/utility_flatseal.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-source "$(dirname "$0")/_base.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
 my:step_begin "install flatseal"
 my:flatpak_install com.github.tchx84.Flatseal
