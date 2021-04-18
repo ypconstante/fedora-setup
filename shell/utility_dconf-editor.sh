cfa6170de9c461e07277caa535404ddc561e3a78
Yuri Pereira Constante <ypconstante@gmail.com>
2021-04-18 01:34:48 -0300

    Add dconf editor

---
+++ shell/utility_dconf-editor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source "$(dirname "$0")/_base.sh"
+
+my:step_begin "install dconf editor"
+my:dnf_install dconf-editor
+my:step_end
+
+my:step_begin "config dconf editor"
+dconf write /ca/desrt/dconf-editor/show-warning false
+my:step_end
