48fd0543fb921431089d83fef4f4d634eac2d41f
Yuri Pereira Constante <ypconstante@gmail.com>
2021-04-29 10:07:44 -0300

    Limit postman access

--- shell/dev_postman.sh
+++ shell/dev_postman.sh
@@ -2,6 +2,16 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
+POSTMAN_APP_NAME=com.getpostman.Postman
+
 my:step_begin "install postman"
-my:flatpak_install com.getpostman.Postman
+my:flatpak_install $POSTMAN_APP_NAME
+my:step_end
+
+my:step_begin "configure postman"
+flatpak override --user $POSTMAN_APP_NAME --reset
+flatpak override --user $POSTMAN_APP_NAME \
+    --nofilesystem=home \
+    --filesystem=~/projects
+rm -rf ~/Postman
 my:step_end
