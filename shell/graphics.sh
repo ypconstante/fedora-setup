94de0c44f3a1a52fabe11a791f56f7c3ebd02ff7
Yuri Pereira Constante <ypconstante@gmail.com>
2021-04-30 21:18:54 -0300

    Enable vaapi

--- shell/graphics.sh
+++ shell/graphics.sh
@@ -6,7 +6,15 @@ my:step_begin "install vulkan"
 my:dnf_install \
     mesa-vulkan-drivers \
     mesa-vulkan-drivers.i686 \
+    vulkan \
     vulkan-loader \
     vulkan-loader.i686 \
     vulkan-tools
+my:step_end
+
+my:step_begin "enable vaapi"
+my:dnf_install \
+    vdpauinfo \
+    libva-vdpau-driver \
+    libva-utils
 my:step_end
