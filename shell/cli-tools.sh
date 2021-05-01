be904df46d7341fd89e04d713f6217a3cf53f334
Yuri Pereira Constante <ypconstante@gmail.com>
2021-04-30 23:31:36 -0300

    Add xdg fix to wget

--- shell/cli-tools.sh
+++ shell/cli-tools.sh
@@ -6,3 +6,8 @@ my:step_begin "install base cli tools"
 my:dnf_install jq
 my:dnf_install inotify-tools
 my:step_end
+
+my_step_begin "fix wget to follow xdg"
+my_append_to_file_if_not_contains "$XDG_CONFIG_HOME/wgetrc" "hsts-file = ${XDG_CACHE_HOME}/wget-hsts"
+rm -f "$HOME/.wget-hsts"
+my_step_end
