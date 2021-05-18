35a4272a43a2510bab777b2baa804c6d46df4dc8
Yuri Pereira Constante <ypconstante@gmail.com>
2021-05-18 19:53:21 -0300

    Fix sound config error

--- shell/sound.sh
+++ shell/sound.sh
@@ -3,7 +3,7 @@
 source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
 my:step_begin "configure sound kernel module"
-my:link_file "$ASSETS_DIR/sound--kernel-module.conf" /etc/modprobe.d/sound.conf
+sudo cp "$ASSETS_DIR/sound--kernel-module.conf" /etc/modprobe.d/sound.conf
 my:step_end
 
 find . -name "sound_*.sh" -print0 | my:run_files
