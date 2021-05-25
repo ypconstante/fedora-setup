e58016233f79614edaec831bb08d4081b0f1d00e
Yuri Pereira Constante <ypconstante@gmail.com>
2021-05-24 23:24:05 -0300

    Fix sound popping caused by default tlp config

--- shell/battery.sh
+++ shell/battery.sh
@@ -5,3 +5,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 my:step_begin "install tlp"
 my:dnf_install tlp tlp-rdw
 my:step_end
+
+my:step_begin "configure tlp"
+sudo cp "$ASSETS_DIR/battery--tlp.conf" /etc/tlp.d/custom.conf
+my:step_end
--- shell/sound.sh
+++ shell/sound.sh
@@ -2,8 +2,4 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
-my:step_begin "configure sound kernel module"
-sudo cp "$ASSETS_DIR/sound--kernel-module.conf" /etc/modprobe.d/sound.conf
-my:step_end
-
 find . -name "sound_*.sh" -print0 | my:run_files
