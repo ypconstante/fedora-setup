adf8b29db93fa91c40286ecc96944525e20d3d0c
Yuri Pereira Constante <ypconstante@gmail.com>
2021-06-02 21:51:21 -0300

    Add virtualization kernel parameters

--- shell/virtualization.sh
+++ shell/virtualization.sh
@@ -14,3 +14,8 @@ sudo modprobe kvm_intel
 sudo modprobe kvm_amd
 sudo usermod -aG libvirt "$USER"
 my:step_end
+
+my:step_begin "virtualization kernel parameters"
+sudo cp "$ASSETS_DIR/virtualization--kernel-parameters.conf" /etc/sysctl.d/98-virtualization.conf
+sudo chmod 644 /etc/sysctl.d/98-virtualization.conf
+my:step_end
