c7b51b8c579c62143b04ee2371020aefc615e746
Yuri Pereira Constante <ypconstante@gmail.com>
2021-11-10 17:59:59 -0300

    Upgrade python to version 10

--- shell/dev_python.sh
+++ shell/dev_python.sh
@@ -15,7 +15,7 @@ my:step_end
 
 my:step_begin "install python"
 my:asdf_add_plugin python
-my:asdf_install_and_set_global python "$(asdf list-all python | grep '^3.9' | grep -v 'dev' | tail -1)"
+my:asdf_install_and_set_global python "$(asdf list-all python | grep '^3.10' | grep -v 'dev' | tail -1)"
 my:step_end
 
 export PIP_REQUIRE_VIRTUALENV=false
