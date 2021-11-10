bb4afd423a463f5e32cdbdf0d8ae7b51f9f9113b
Yuri Pereira Constante <ypconstante@gmail.com>
2021-11-10 15:36:35 -0300

    Move starship to a separate script

--- shell/terminal_fish.sh
+++ shell/terminal_fish.sh
@@ -11,11 +11,6 @@ my:step_begin "reset config"
 rm -rf "${XDG_CONFIG_HOME}/fish"
 my:step_end
 
-my:step_begin "install starship"
-my:dnf_install starship
-my:link_file "$ASSETS_DIR/terminal_fish--starship.toml" "$XDG_CONFIG_HOME/starship.toml"
-my:step_end
-
 my:step_begin "add asdf"
 my:link_file "$ASDF_DIR/completions/asdf.fish" "${XDG_CONFIG_HOME}/fish/completions/asdf.fish"
 my:link_file "$ASDF_DATA_DIR/plugins/java/set-java-home.fish" "${XDG_CONFIG_HOME}/fish/conf.d/asdf-java.fish"
