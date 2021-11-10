b264560a8c22170471e104305325c6fc23e043f4
Yuri Pereira Constante <ypconstante@gmail.com>
2021-11-10 17:59:29 -0300

    Upgrade node to version 16

--- shell/dev_node.sh
+++ shell/dev_node.sh
@@ -6,8 +6,8 @@ my:step_begin "install node"
 my:asdf_add_plugin nodejs
 my:echo_substep "Importing node keys"
 $ASDF_DATA_DIR/plugins/nodejs/bin/import-release-team-keyring &> /dev/null
-my:asdf_install_and_set_global nodejs "$(asdf list-all nodejs | grep '^12' | tail -1)"
 my:asdf_install_and_set_global nodejs "$(asdf list-all nodejs | grep '^14' | tail -1)"
+my:asdf_install_and_set_global nodejs "$(asdf list-all nodejs | grep '^16' | tail -1)"
 my:step_end
 
 my:step_begin "install yarn"
