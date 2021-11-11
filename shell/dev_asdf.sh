5105c1fa72b7b3cf2dfa327b9755dd9fbd14025e
Yuri Pereira Constante <ypconstante@gmail.com>
2021-11-11 01:10:24 -0300

    Use head version of asdf

--- shell/dev_asdf.sh
+++ shell/dev_asdf.sh
@@ -4,7 +4,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
 my:step_begin "install asdf"
 my:git_clone https://github.com/asdf-vm/asdf.git "$ASDF_DIR" "last-tag"
-asdf update
+asdf update --head
 my:step_end
 
 my:step_begin "config asdf"
