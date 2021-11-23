24a1c5a147cc33d1c0aebacf83fe7d6566c509c1
Yuri Pereira Constante <ypconstante@gmail.com>
2021-11-22 22:47:23 -0300

    Disable ssh access

--- shell/network.sh
+++ shell/network.sh
@@ -11,6 +11,10 @@ sudo mkdir -p /etc/systemd/resolved.conf.d
 sudo cp "$ASSETS_DIR/network--systemd-resolved.conf" /etc/systemd/resolved.conf.d/custom.conf
 my:step_end
 
+my:step_begin "configure firewall"
+sudo firewall-cmd --zone=public --permanent --remove-service ssh
+my:step_end
+
 my:step_begin "disable unnecessary services"
 sudo systemctl disable ModemManager.service NetworkManager-wait-online.service
 sudo systemctl mask remote-fs.target
