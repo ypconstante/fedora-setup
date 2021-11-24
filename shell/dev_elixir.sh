ed678b8cfdc6a381d7920eb682329281d8d1efd2
Yuri Pereira Constante <ypconstante@gmail.com>
2021-11-24 19:15:37 -0300

    Add elixir and erlang

---
+++ shell/dev_elixir.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
+
+my:step_begin "install dependencies"
+my:toolbox-run \
+    sudo dnf group install -y -q --with-optional \
+        'Development Tools' \
+        'C Development Tools and Libraries'
+my:toolbox-run \
+    my:dnf_install \
+        ncurses-devel \
+        openssl-devel
+my:step_end
+
+my:step_begin "install erlang"
+my:asdf_add_plugin erlang
+my:toolbox-run \
+    my:asdf_install_and_set_global erlang latest:24
+my:step_end
+
+my:step_begin "install elixir"
+my:asdf_add_plugin elixir
+my:toolbox-run \
+    my:asdf_install_and_set_global elixir "$(asdf list-all elixir 1.12 | grep otp-24 | grep -v rc | tail -1)"
+my:step_end
