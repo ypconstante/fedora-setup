49dade233f7bea795b7b9c8bd248601672bbff9c
Yuri Pereira Constante <ypconstante@gmail.com>
2021-11-26 01:02:22 -0300

    Migrate from docker to podman

--- shell/dev_containers.sh
+++ shell/dev_containers.sh
@@ -2,24 +2,17 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
-my:step_begin "install docker"
-my:dnf_add_repo https://download.docker.com/linux/fedora/docker-ce.repo
-my:dnf_install docker-ce docker-ce-cli docker-compose containerd.io
+my:step_begin "install podman and docker-compose"
+my:dnf_install \
+    podman \
+    podman-docker \
+    podman-compose \
+    docker-compose \
+    containerd.io
 my:step_end
 
-
-my:step_begin "configure docker"
-# https://docs.docker.com/engine/security/rootless/
-my:dnf_install fuse-overlayfs iptables
-
-## workaround - https://github.com/moby/moby/issues/41230
-my:dnf_install policycoreutils-python-utils && sudo semanage permissive -a iptables_t
-
-dockerd-rootless-setuptool.sh install
-
-my:link_file "$ASSETS_DIR/dev_containers--docker-daemon.json" /etc/docker/daemon.json
-systemctl --user enable docker.service
-sudo loginctl enable-linger "$USER"
+my:step_begin "enable podman with docker-compose"
+systemctl --user enable podman.socket
 my:step_end
 
 my:step_begin "fish config"
