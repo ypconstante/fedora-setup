8126204f56441b34a574751d1b159a8ccdcd16c5
Yuri Pereira Constante <ypconstante@gmail.com>
2021-11-26 11:18:19 -0300

    Disable watchdog

--- shell/boot.sh
+++ shell/boot.sh
@@ -6,8 +6,13 @@ my:step_begin "configure grub"
 sudo grub2-editenv /boot/efi/EFI/fedora/grubenv create
 sudo cp "$ASSETS_DIR/boot--grub-config.sh" /etc/grub.d/99_custom
 sudo chmod +x /etc/grub.d/99_custom
-my:append_to_file_if_not_contains /etc/default/grub 'GRUB_CMDLINE_LINUX_DEFAULT="fbcon=nodefer"'
+my:append_to_file_if_not_contains /etc/default/grub 'GRUB_CMDLINE_LINUX_DEFAULT="fbcon=nodefer nowatchdog nmi_watchdog=0"'
 sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
+my:step_end
+
+my:step_begin "disable watchdog"
+my:append_to_file_if_not_contains /etc/modprobe.d/watchdog-blacklist.conf 'blacklist iTCO_wdt'
+my:append_to_file_if_not_contains /etc/modprobe.d/watchdog-blacklist.conf 'blacklist iTCO_vendor_support'
 my:step_end
 
 my:step_begin "change plymouth theme"
