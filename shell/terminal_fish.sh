04ff420139971f690eb424975fb0912ba3835937
Yuri Pereira Constante <ypconstante@gmail.com>
2021-12-02 18:35:39 -0300

    Migrate custom fish configs to plugin

--- shell/terminal_fish.sh
+++ shell/terminal_fish.sh
@@ -7,24 +7,28 @@ my:dnf_install fish
 mkdir -p "$XDG_CONFIG_HOME/fish/completions"
 my:step_end
 
-my:step_begin "reset config"
-rm -rf "$XDG_CONFIG_HOME/fish"
+if fish -c "! functions --query fisher"; then
+    my:step_begin "install fisher"
+    mkdir -p "$HOME/projects/personal/fish-local/conf.d"
+    mkdir -p "$HOME/projects/personal/fish-local/completions"
+    mkdir -p "$HOME/projects/personal/fish-local/functions"
+
+    fish -c "curl -sSL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
+    my:step_end
+fi
+
+my:step_begin "add starship initialization"
+/usr/bin/starship init fish --print-full-init > "$HOME/projects/personal/fish-local/conf.d/starship.fish"
 my:step_end
 
-my:step_begin "install and configure fisher"
-mkdir -p "$HOME/projects/personal/fish-local/conf.d"
-mkdir -p "$HOME/projects/personal/fish-local/completions"
-mkdir -p "$HOME/projects/personal/fish-local/functions"
-my:link_file "$ASSETS_DIR/terminal_fish--fisher_plugins" "$XDG_CONFIG_HOME/fish/fish_plugins"
+my:step_begin "install fisher plugins"
+# disable conflict check
+sed -Ei '/#/!s|(.*set --append conflict_files)|#\1|' "$XDG_CONFIG_HOME/fish/functions/fisher.fish"
 
-fish -c "curl -sSL https://git.io/fisher | source && fisher update"
-cp -r "$ASSETS_DIR/terminal_fish--config/." "$XDG_CONFIG_HOME/fish"
+my:link_file "$ASSETS_DIR/terminal_fish--fisher_plugins" "$XDG_CONFIG_HOME/fish/fish_plugins"
+fish -c "fisher update"
 my:step_end
 
 my:step_begin "init universal config"
 find "$ASSETS_DIR/terminal_fish--config/universal-conf.d/" -name '*.fish' -exec fish '{}' \;
-my:step_end
-
-my:step_begin "add starship initialization"
-/usr/bin/starship init fish --print-full-init > "$XDG_CONFIG_HOME/fish/conf.d/starship.fish"
 my:step_end
