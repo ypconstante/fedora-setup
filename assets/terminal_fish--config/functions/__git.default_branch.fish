aa69b19a423c88eeaf2f7c0fba1265a1dcd13da8
Yuri Pereira Constante <ypconstante@gmail.com>
2021-12-02 14:28:33 -0300

    Remove duplicated code on git functions

--- assets/terminal_fish--config/functions/__git.default_branch.fish
+++ assets/terminal_fish--config/functions/__git.default_branch.fish
@@ -1,5 +1,5 @@
 function __git.default_branch -d "Fallback to main when master branch is not found"
   # Silently fail when not in git directory
   command git rev-parse --git-dir &>/dev/null; or return
-  git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'
+  git-main-branch
 end
