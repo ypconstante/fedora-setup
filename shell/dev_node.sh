79661dc88d59b04a9f6cc565d620d2f0f137b016
Yuri Pereira Constante <ypconstante@gmail.com>
2022-02-24 20:52:40 -0300

    Add node configuration

--- shell/dev_node.sh
+++ shell/dev_node.sh
@@ -7,6 +7,10 @@ my:asdf_add_plugin nodejs
 my:asdf_install_and_set_global nodejs latest:14
 my:asdf_install_and_set_global nodejs latest:16
 
+my:step_begin "configure node"
+my:append_to_file_if_not_contains "$NPM_CONFIG_USERCONFIG" 'engine-strict = true'
+corepack enable
+
 my:step_begin "install yarn"
 my:asdf_add_plugin yarn
 my:asdf_install_and_set_global yarn latest
