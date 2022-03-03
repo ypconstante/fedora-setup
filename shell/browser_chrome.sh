4ff9eaa14cb0974a2806cb2929e4bd3074f8e0ac
Yuri Pereira Constante <ypconstante@gmail.com>
2022-03-03 11:41:12 -0300

    Migrate to Ungoogled Chromium

--- shell/browser_chrome.sh
+++ shell/browser_chrome.sh
@@ -7,9 +7,10 @@ sudo dnf config-manager --set-enabled google-chrome
 my:dnf_install google-chrome-stable
 
 my:step_begin "install chromium"
-my:dnf_install chromium
+my:flatpak_install com.github.Eloston.UngoogledChromium
 
-my:step_begin "add temporary chromium to menu"
-menu_file="$XDG_DATA_HOME/applications/chromium-temporary.desktop"
-my:link_file "$ASSETS_DIR/browser_chrome--temp-profile.desktop" "$menu_file"
-chmod +x "$menu_file"
+my:step_begin "configure"
+flatpak override --user com.github.Eloston.UngoogledChromium --reset
+flatpak override --user com.github.Eloston.UngoogledChromium \
+    --nofilesystem=home \
+    --persist=xdg-config
